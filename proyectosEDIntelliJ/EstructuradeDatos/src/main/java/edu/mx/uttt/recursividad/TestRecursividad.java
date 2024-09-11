dda2d5dddf17bce044a17bfdc93045bb46fed240
Jose Luis Gallardo <gallardohjl2@gmail.com>
2024-09-11 14:27:37 -0600

    Logro: Se realizo el programa iterativo y Recursivo del Factorial de un número

--- proyectosEDIntelliJ/EstructuradeDatos/src/main/java/edu/mx/uttt/recursividad/TestRecursividad.java
+++ proyectosEDIntelliJ/EstructuradeDatos/src/main/java/edu/mx/uttt/recursividad/TestRecursividad.java
@@ -11,7 +11,9 @@ public class TestRecursividad {
          String menu= "Menu Principal\n" +
                  "1)Método Iterativo\n" +
                  "2)Método Recursivo\n" +
-                 "3)Salir\n" +
+                 "3)Factorial Iterativo\n" +
+                 "4)Factorial Recursivo\n" +
+                 "5)Salir\n" +
                  "Elige la Opción";
          return JOptionPane.showInputDialog(menu);
     }
@@ -19,7 +21,10 @@ public class TestRecursividad {
         boolean sentinel = true;
         String opcion = "";
         Operaciones op = new Operaciones();
+        Factorial f = null;
+        long r = 0;
         int n = 0;
+
         do {
             opcion = menu();
 
@@ -34,7 +39,28 @@ public class TestRecursividad {
                     op.setN(n);
                     op.imprimir(op.getN());
                     break;
-                case "3": //Salir
+                case "3":
+                    //Factorial
+                    opcion = JOptionPane.
+                            showInputDialog(Menus.menuFactorial());
+                    f = new Factorial();
+                    n = Integer.parseInt(JOptionPane.showInputDialog
+                            ("Introduce el valor del factorial a calcular"));
+                    f.setN(n);
+                    r = f.factorialI(opcion);
+                    JOptionPane.showMessageDialog
+                            (null,"f( " + f.getN() + ")=  " + r);
+                    break;
+                case "4":
+                    f = new Factorial();
+                    n = Integer.parseInt(JOptionPane.showInputDialog
+                            ("Introduce el valor del factorial a calcular"));
+                    f.setN(n);
+                    r = f.factR(f.getN());
+                    JOptionPane.showMessageDialog
+                            (null,"f( " + f.getN() + ")=  " + r);
+                    break;
+                case "5": //Salir
                     sentinel = false;
                     JOptionPane.showMessageDialog(null, "I'll be back");
                     break;
